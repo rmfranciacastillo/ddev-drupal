01cd71b56759fa35012d1ac5ec3353ad46695a26
Renato Francia <rmfranciacastillo@gmail.com>
2024-11-13 10:11:44 -0500

    Added: JS test

---
+++ web/modules/custom/sample/src/js/example.js
@@ -0,0 +1,7 @@
+// src/js/example.js
+function addNumbers(a, b) {
+  return a + b;
+}
+
+export { addNumbers };
+
---
+++ web/modules/custom/sample/tests/js/myScript.spec.js
@@ -0,0 +1,22 @@
+// tests/jasmine/spec/exampleSpec.js
+//import { addNumbers } from '../../../../src/js/example.js';
+
+describe("A suite", function() {
+    it("contains a spec with an expectation", function() {
+        expect(true).toBe(true);
+    });
+});
+
+/*
+describe("addNumbers", function () {
+  it("should add two numbers correctly", function () {
+    expect(addNumbers(1, 2)).toBe(3);
+    expect(addNumbers(-1, 1)).toBe(0);
+  });
+
+  it("should return NaN if a non-number is passed", function () {
+    expect(addNumbers(1, "a")).toBeNaN();
+  });
+});
+
+*/
